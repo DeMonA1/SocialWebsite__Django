048031aeea67f51e03895d0d119332cca0d0ec42
DeMonA1 <demonagrail1@gmail.com>
2024-10-20 17:47:20 +0300

    ContentType objects

--- bookmarks/test_commands.py
+++ bookmarks/test_commands.py
@@ -3,3 +3,12 @@
 from django.contrib.auth.models import User
 user = User.objects.latest('id')
 str(user.get_absolute_url())
+
+
+# interact with ContentType objects
+from django.contrib.contenttypes.models import ContentType
+image_type = ContentType.objects.get(app_label='images', model='image')
+image_type
+image_type.model_class()
+from images.models import Image
+ContentType.objects.get_for_model(Image)
